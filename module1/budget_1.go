b474835b621876969de37207100c8f4472d4e157
Joe Vieira do Carmo <Joe.Carmo@niceincontact.com>
2021-09-09 16:56:51 -0400

    Module complete

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,12 @@
 package module1
 
 // Budget stores budget information
-
+struct Budget {
+	Max float32,
+	Items []Item,
+}
 // Item stores item information
+struct Item {
+	Description string,
+	Price float32,
+}
